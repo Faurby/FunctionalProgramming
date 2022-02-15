249b9844a74fca48d4069550c8db1d68eefdb8f9
Faurby <lasse_fk@hotmail.com>
2022-02-15 22:50:52 +0100

    yay did 3.3

--- Assignment3/Assignment3.fsx
+++ Assignment3/Assignment3.fsx
@@ -1,8 +1,12 @@
 // Arithmetic Expression can either be an integer,
 // or addition, subtraction, multiplication of two aExp
+
+// Setup
 type aExp =
   | N of int // Integer value
   | V of string // Variable
+  | WL // Length of the word
+  | PV of aExp // Point value of character at specific word index
   | Add of aExp * aExp // Addition
   | Sub of aExp * aExp // Subtraction
   | Mul of aExp * aExp // Multiplication
@@ -19,6 +23,12 @@ let a5 = N 4 .+. (N 2 .*. N 34)
 let a6 = V "x"
 let a7 = N 4 .+. (V "y" .-. V "z")
 
+let arithSingleLetterScore = PV (V "_pos_") .+. (V "_acc_")
+let arithDoubleLetterScore = ((N 2) .*. PV (V "_pos_")) .+. (V "_acc_")
+let arithTripleLetterScore = ((N 3) .*. PV (V "_pos_")) .+. (V "_acc_")
+let arithDoubleWordScore = N 2 .*. V "_acc_"
+let arithTripleWordScore = N 3 .*. V "_acc_"
+
 // Exercise 3.1
 let rec arithEvalSimple =
   function
@@ -28,23 +38,28 @@ let rec arithEvalSimple =
   | Mul (x,y) -> (arithEvalSimple x) * (arithEvalSimple y)
   | _ -> invalidArg "error" "invalid argument"
 
-let validString =
-  function
-  | Some x -> x
-  | None -> 0
 
-let rec arithEvalState =
-  function
+// Exercise 3.2
+let rec arithEvalState (a: aExp) (s: Map<string, int>) =
+  match a with
   | N a -> a
-  | Add (x,y) -> arithEvalState x
-  | Sub (x, y) -> arithEvalState x
-  | V a -> validString (Map.tryFind a)
+  | Add (x,y) -> (arithEvalState x s) + (arithEvalState y s)
+  | Sub (x, y) -> (arithEvalState x s) - (arithEvalState y s)
+  | V a -> s |> Map.tryFind a |> Option.defaultValue 0 // wizardy :o
 
-let rec arithEvalState2 =
-  function
-  | a -> if (Map.containsKey a) then a else 0
+// Exercise 3.3
+type word = (char * int) list
+let hello = ('H', 4)::('E', 1)::('L', 1)::('L', 1)::('O', 1)::[]
 
-let rec arithEvalState2 =
-  function
-  | a when Map.containsKey a -> a
-  | _ -> 0
+
+let rec arithEval (a: aExp) (w: word) (s: Map<string, int>) =
+  match a with
+  | WL -> w.Length
+  | PV x when s.IsEmpty -> snd w.[arithEvalSimple x]
+  | PV x -> arithEval x w s
+  | V "_pos_" -> snd w.[s.TryFind "_pos_" |> Option.defaultValue 0]
+  | V "_acc_" -> s |> Map.tryFind "_acc_" |> Option.defaultValue 0
+  | Add (x, y) -> (arithEval x w s) + (arithEval y w s)
+  | Sub (x, y) -> (arithEval x w s) - (arithEval y w s)
+  | Mul (x, y) -> (arithEval x w s) * (arithEval y w s)
+  | N x -> x
